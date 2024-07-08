350925cdbf6fec045b3b1123bdbae97067bd079f
alichman <alichman@uoguelph.ca>
2024-07-08 00:11:23 -0400

    Add basic stuff for title page

--- readwise-report/src/App.tsx
+++ readwise-report/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="TitlePage">
+                <div className="TitleInfo">
+                    <h1>
+                        <strong>The ReadWise Report</strong>
+                    </h1>
+                    <h2>
+                        Brought to you by Andrew Lichmanov
+                    </h2>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
