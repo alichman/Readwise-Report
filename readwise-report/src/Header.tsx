ddace6b1126654e199e08f6a15d2143e6116c908
Andrew Lichmanov <andrewlichmanov@gmail.com>
2024-07-18 10:58:07 -0400

    added silly title for layout

---
+++ readwise-report/src/Header.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export const Header = () => {
+
+    return <header className="App-header">
+        <div className="headerTextDiv">
+        <h1>The Readwise Report</h1>
+        <p>A summary of my experience working at the company,
+            what I've learned, and what I look forward to
+            for my next term with Readwise.</p>
+        </div>
+    </header>
+
+
+}
