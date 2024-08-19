7277e3ce93aa0ff5b0c969282b328fa76bc94e72
Andrew Lichmanov <andrewlichmanov@gmail.com>
2024-08-18 22:41:32 -0400

    Semi completed title page

--- readwise-report/src/App.tsx
+++ readwise-report/src/App.tsx
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className="App">
         <Header/>
-      <div className="simpleSegment">
+        <div className="simpleSegment">
           <h1> What is Readwise </h1>
           <p> Readwise is a start-up focused on creating the ultimate
           reading apps. Their main product, and the one I have been working
           on, is the Readwise Reader - a tool that provides features typically
           restricted to PDFs to everything from eBooks to emails and twitter threads.</p>
-      </div>
+        </div>
       <ImageFlexRow/>
       <div className="simpleSegment">
         <h1> My role</h1>
@@ -23,11 +23,8 @@ function App() {
             with important, long-term projects. This allows many bugs and smaller features,
             such as animations and log-out screens, to be ignored for too long.<br/>
             That's where an intern like me comes in!<br/><br/>
-            A summary of my contributions can be found below.
           </p>
       </div>
-        <ImageFlexRow/>
-        <GoalsSection/>
     </div>
   );
 }
--- readwise-report/src/Header.tsx
+++ readwise-report/src/Header.tsx
@@ -1,20 +1,19 @@
 import './App.css'
+import './Header.css'
 import {useEffect, useState} from "react";
+import logo from './assets/rw-logo-dark.png';
 
 export const Header = () => {
-    const [doAnimation, setDoAnimation] = useState(true)
+    const [isVisible, setIsVisible] = useState(false);
     useEffect(() => {
-        setDoAnimation(false);
+        setIsVisible(true)
     }, []);
 
-    const className = "App-header " //+ doAnimation? "headerTextDivOnLoad":''
-
-    return <header className={className}>
-        <div className="headerTextDiv">
+    return <header className={`App-header`}>
+        <img src={logo} alt="readwise-logo" className={`logo ${isVisible ? 'imageOnLoad' : ''}`} />
+        <div className={`headerTextDiv ${isVisible ? 'headerTextOnLoad' : ""}`}>
         <h1>The Readwise Report</h1>
-        <p>A summary of my experience working at the company,
-            what I've learned, and what I look forward to
-            for my next term with Readwise.</p>
+        <p>A summary of my experience with the company <br/> brought to you by Andrew Lichmanov</p>
         </div>
     </header>
 
