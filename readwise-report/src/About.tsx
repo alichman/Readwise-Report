dafe0fd30e14a35cc98cf627f65a62700d9fdde9
Andrew Lichmanov <andrewlichmanov@gmail.com>
2024-08-26 08:10:50 -0400

    about stuff

--- readwise-report/src/About.tsx
+++ readwise-report/src/About.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import './About.css'
 import phoneImage from './assets/rw-phone.png'
+import ipadImage from './assets/rw-ipad.png'
 
 export const About = () => (
     <div className="about">
-        <img src={phoneImage} className="aboutImg" />
+        <div className="imgDiv">
+            <img src={ipadImage} className="ipadImg" alt='reader app on ipad'/>
+            <img src={phoneImage} className="phoneImg" alt = 'reader app on iphone'/>
+        </div>
         <div className="aboutText">
             <h1>What is Readwise</h1>
             <p>
-                Readwise is a Toronto-based start-up aiming to develop the ultimate reading apps.
+            Readwise is a Toronto-based start-up aiming to develop the ultimate reading apps.
                 Their main product, on which I've had the privilege to work on, is the Readwise Reader -
                 A power-user focused app packed with features designed for productivity.<br/><br/>
                 In my opinion, they most definitely have the potential to dethrone Amazon and Adobe
